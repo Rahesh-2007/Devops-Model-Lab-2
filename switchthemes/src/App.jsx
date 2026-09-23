c808b3b00ff6ecbd0b45e04e388950abcb5b19e7
unknown <Administrator@APJ-P90-IP61.dc.cds>
2026-09-23 16:50:20 +0530

    background change

---
+++ switchthemes/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+function App() {
+  const [isDark, setIsDark] = useState(false);
+  return (
+    <div
+      style={{
+        backgroundColor: isDark ? "black" : "white",
+        color:  isDark? "white" : "black",
+      }}
+    >
+      <div className="thrla">
+        <h1>{isDark ? "Dark Theme" : "Light Theme"}</h1>
+        <button onClick={() => setIsDark(!isDark)}>
+          Switch to {isDark ? "Light" : "Dark"} Theme
+        </button>
+      </div>
+    </div>
+  );
+}
+export default App;
